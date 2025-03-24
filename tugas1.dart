d768f73cef362981ccb6569a4e9765b6d79386a1
NickoSugiarto11 <nicko9995sgt@gmail.com>
2025-03-24 22:20:52 +0700

    Create tugas1.dart

---
+++ tugas1.dart
@@ -0,0 +1,34 @@
+void insertionSortDescending<T extends Comparable<T>>(List<T> arr) {
+  for (int i = arr.length - 1; i >= 0; i--) {
+    for (int j = i + 1, k = i; j < arr.length; j++) {
+      if (arr[j].compareTo(arr[k]) < 0) {
+        break;
+      } else {
+        T temp = arr[k];
+        arr[k] = arr[j];
+        arr[j] = temp;
+        k = j;
+      }
+    }
+  }
+}
+
+void display<T>(List<T> data) {
+  for (T obj in data) {
+    print('$obj ');
+  }
+  print('');
+}
+
+void main() {
+  List<num> data = [5, 7, 3, 19, 22, 16, 41];
+  print('Data Sebelum Pengurutan:');
+  display(data);
+
+  DateTime start = DateTime.now();
+  insertionSortDescending<num>(data);
+  Duration elapsedTime = DateTime.now().difference(start);
+  print('Data Setelah Pengurutan (Descending):');
+  display(data);
+  print('Waktu: $elapsedTime');
+}
